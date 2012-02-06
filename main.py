c060e56d205bf220e891199e670b2bdc2b225a7c
Dominic Smith <code@smithamax.me>
2012-02-06 19:54:02 +1100

    init commit
    All the features I want for the moment are implemented.

---
+++ main.py
@@ -0,0 +1,179 @@
+import Image
+import os
+import argparse
+
+rots = {
+    1: 0,
+    3: 180,
+    6: 270,
+    8: 90
+}
+
+
+def resize_image(img, longside=960, width=None, height=None):
+    width, height = img.size
+    if width < height:
+        newsize = (int((float(width) / height) * longside), longside)
+    else:
+        newsize = (longside, int((float(height) / width) * longside))
+    return img.resize(newsize, Image.ANTIALIAS)
+
+
+def parse_position(posStr, img, mark):
+    if posStr.find(',') != -1:
+        x, y = posStr.split(',')
+        pos = (int(x), int(y))
+
+    else:
+        bottom = img.size[1] - mark.size[1]
+        right = img.size[0] - mark.size[0]
+
+        if posStr == 'br':
+            pos = (right, bottom)
+        if posStr == 'bl':
+            pos = (0, bottom)
+        if posStr == 'tr':
+            pos = (right, 0)
+        if posStr == 'tl':
+            pos = (0, 0)
+
+    return pos
+
+
+def watermark_image(img, mark, pos):
+    if type(pos) == str:
+        pos = parse_position(pos, img, mark)
+    img.paste(mark, pos, mark)
+
+
+def save_image(img, imgpath, prefix=None, outdir=None):
+    name = os.path.basename(imgpath)
+    if outdir == None:
+        outdir = os.path.dirname(imgpath)
+
+    if prefix:
+        name = prefix + name
+
+    img.save(os.path.join(outdir, name))
+
+
+def process_images(args):
+    watermarks = []
+
+    if args.watermarks:
+        for markpath, pos in args.watermarks:
+            watermarks.append((Image.open(markpath), pos))
+
+    for imagepath in args.images:
+        img = Image.open(imagepath)
+        img = fix_rotation(img)
+
+        if args.width:
+            img = resize_image(img, width=args.width)
+        elif args.longside:
+            img = resize_image(img, longside=args.longside)
+        elif args.height:
+            img = resize_image(img, height=args.height)
+        else:
+            img = resize_image(img)
+
+        for mark, pos in watermarks:
+            watermark_image(img, mark, pos)
+
+        if args.show:
+            show_image(img)
+
+        if args.verbose:
+            print imagepath
+
+        save_image(img, imagepath, args.prefix, args.outdir)
+
+
+def get_orientation(img):
+    # rotation refrence http://sylvana.net/jpegcrop/exif_orientation.html
+    # EXIF usage http://stackoverflow.com/a/765403/730767
+    info = None
+
+    if img._getexif:
+        info = img._getexif()
+
+    if info:
+        for tag, value in info.items():
+            if tag == 274:
+                return value
+
+    return None
+
+
+def fix_rotation(img):
+    o = get_orientation(img)
+    if o:
+        return img.rotate(rots[o])
+    else:
+        return img
+
+
+def show_image(img):
+    img.show()
+
+
+def main():
+    parser = argparse.ArgumentParser(description='')
+
+    resizers = parser.add_mutually_exclusive_group()
+
+    resizers.add_argument('--width',
+            metavar='pixels', type=int,
+            help=''
+            )
+
+    resizers.add_argument('--height',
+            metavar='pixels', type=int,
+            help=''
+            )
+
+    resizers.add_argument('-l', '--longside',
+            metavar='pixels', type=int,
+            dest='longside',
+            help=''
+            )
+
+    parser.add_argument('-w', '--watermark',
+            metavar=('img', 'pos'),
+            nargs=2, action='append',
+            dest='watermarks',
+            help=''
+            )
+
+    parser.add_argument('-o', '--output-dir',
+            metavar=('dir'),
+            dest='outdir',
+            help=''
+            )
+
+    parser.add_argument('-s', '--show',
+            action='store_true',
+            help=''
+            )
+
+    parser.add_argument('-v', '--verbose',
+            action='store_true',
+            help=''
+            )
+
+    parser.add_argument('-p', '--prefix',
+            metavar=('str'),
+            help=''
+            )
+
+    parser.add_argument('images',
+            nargs='*',
+            help='images to resize'
+            )
+
+    args = parser.parse_args()
+    print args
+    process_images(args)
+
+if __name__ == '__main__':
+    main()
